ee281a51fa7acf4767a012818d95034b8ae31eb0
Aleksandr Roslyakov <torpid@gmail.com>
2018-02-24 03:24:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function theBeatlesPlay (musician, instruments){
   var finish = []
   for (var i = 0; i < musician.length; i++) {
     var finish = [...musician.slice(i), ...instruments.slice(i)]
-
+    return finish
   }
 }
